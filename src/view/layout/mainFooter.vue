354c8dceeb2271e72552f8b1d9880158ab9ea9c7
bruce <shiyj@5mutian.com>
2017-07-31 09:23:38 +0800

    添加footer

--- src/view/layout/mainFooter.vue
+++ src/view/layout/mainFooter.vue
@@ -1,9 +1,13 @@
 <template>
-  <div id="mainFooter">
-    footer
+  <div class="main-footer" id="mainFooter">
+    <h1>轩辕科技</h1>
+    <h2>&#169;Copyright 2017-3000</h2>
+    <h2>轩辕科技 ALL RIGHTS RESERVED</h2>
+    <h3>备案号：沪ICP 备11087258号-2</h3>
   </div>
 </template>
 <script>
+import '@/assets/styles/layout/mainFooter.scss'
 export default {
   name: 'mainFooter',
   data: () => ({
@@ -11,5 +15,3 @@ export default {
   })
 }
 </script>
-<style lang="scss" scoped>
-</style>
