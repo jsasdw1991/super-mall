5a3dba78e8918cf3ab853b8f358b5c9e96b1fdec
bruce <shiyj@5mutian.com>
2017-07-31 10:01:59 +0800

    修改文件内容

--- src/view/layout/mainFooter.vue
+++ src/view/layout/mainFooter.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="main-footer" id="mainFooter">
-    <h1>轩辕科技</h1>
-    <h2>&#169;Copyright 2017-3000</h2>
+    <h1>&#169;Copyright 2017-3000</h1>
     <h2>轩辕科技 ALL RIGHTS RESERVED</h2>
     <h3>备案号：沪ICP 备11087258号-2</h3>
   </div>
 </template>
+
 <script>
 import '@/assets/styles/layout/mainFooter.scss'
 export default {
